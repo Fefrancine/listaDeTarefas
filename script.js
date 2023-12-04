99755317f46f9dce37456859a89e3a5dfd7d3b32
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-12-04 16:45:33 -0300

    lista de tarefas finalizadas

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-let pendentes = document.getElementById("tarefasPendentes");
-let finalizadas = document.getElementById("tarefasFinalizadas");
-let descricao = document.getElementById("oqueInserir");
+let listaPendentes = document.getElementById("tarefasPendentes");
+let listaFinalizadas = document.getElementById("tarefasFinalizadas");
+let buttonAdicionar = document.getElementById("buttonAdicionar");
+let input = document.getElementById("inputAdicionar");
 
-descricao.addEventListener("keypress", function(e) {
+
+// 
+function adicionarTarefa() {
+    let li = document.createElement("li");
+    li.innerHTML = input.value; // o que o usuario colocou no input, virou li
+
+    let button = document.createElement("button");
+    button.innerHTML = 'Finalizar';
+
+    button.addEventListener("click", () => {
+        finalizarTarefa(li, button);
+    })
+
+    li.appendChild(button);
+    listaPendentes.appendChild(li); // bt como filho da li
+}
+
+// limpa o input para add outras tarefas
+function limparInput() {
+    input.value = '';
+}
+
+//
+function finalizarTarefa(tarefa, botao) {
+    let removerBotao = tarefa.removeChild(botao); // remove o botão
+    let cloneTarefa = tarefa.cloneNode(true); // clone para colocar na lista de baixo
+    listaFinalizadas.appendChild(cloneTarefa); // clone na lista de finalizadas
+    tarefa.remove();
+}
+
+input.addEventListener("keydown", function(e) { 
     if (e.key == "Enter") {
         adicionarTarefa();
+        limparInput()
     }
 })
 
-function adicionarTarefa() {
-    
-}
+buttonAdicionar.addEventListener("click", () => {
+    adicionarTarefa();
+    limparInput();
+});
